12793540ffe9111e1db7cf7a3e0588a31b07087b
barath682 <barathkumarr22@gmail.com>
2025-01-09 18:39:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Dark/Light Mode Toggle
-const themeToggle = document.getElementById('theme-toggle');
-themeToggle.addEventListener('click', () => {
-  document.body.dataset.theme =
-    document.body.dataset.theme === 'dark' ? 'light' : 'dark';
-});
