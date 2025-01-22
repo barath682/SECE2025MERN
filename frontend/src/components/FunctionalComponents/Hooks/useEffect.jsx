1b53618bdf2d2f9dcece5ac1f3a19425861daf7b
barath682 <barathkumarr22@gmail.com>
2025-01-22 17:58:07 +0530

    day9

--- frontend/src/components/FunctionalComponents/Hooks/useEffect.jsx
+++ frontend/src/components/FunctionalComponents/Hooks/useEffect.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react"
+import { useState ,useEffect} from "react"
 import NavBar from "../navbar"
 
 const UseEffect=()=>{
@@ -6,12 +6,13 @@ const UseEffect=()=>{
     useEffect(()=>{
         console.log(text)
     },[text])
-        
     return (
+        
         <div>
-            <h2>This is UseEffect example</h2>
+            
+            <h2>this is useeffect example</h2>
             <input type="number" value={text} onChange={(e)=>{setText(e.target.value)}} />
-            <h2>The text is {text}</h2>
+            <h2>the text is {text}</h2>
         </div>
     )
 }
