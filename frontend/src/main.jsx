fd0561a6b745da3bbcc35e9f54f6fed789a968df
barath682 <barathkumarr22@gmail.com>
2025-01-24 11:32:08 +0530

    day11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,42 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './components/FunctionalComponents/home.jsx'
-import About from './components/FunctionalComponents/about.jsx'
-import Gallery from './components/FunctionalComponents/gallary.jsx'
-import Contact from './components/FunctionalComponents/contact.jsx'
-import Signup from './components/FunctionalComponents/signup.jsx'
-import NavBar from './components/FunctionalComponents/navBar.jsx'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import UseState from './components/FunctionalComponents/Hooks/useState.jsx'
-import UseEffect from './components/FunctionalComponents/Hooks/useEffect.jsx'
-import UseEffectApi from './components/FunctionalComponents/Hooks/useEffectApi.jsx'
-import UseReducer from './components/FunctionalComponents/Hooks/useReducer.jsx'
-import UseEffectApiImage from './components/FunctionalComponents/Hooks/useEffectApiimage.jsx'
-import UseRef from './components/FunctionalComponents/Hooks/useRef.jsx'
-import UseMemo from './components/FunctionalComponents/Hooks/useMemo.jsx'
-import UseCallBack from './components/FunctionalComponents/Hooks/useCallback.jsx'
+// Importing Components
+import Home from './components/FunctionalComponents/home.jsx';
+import About from './components/FunctionalComponents/about.jsx';
+import Gallery from './components/FunctionalComponents/gallary.jsx';
+import Contact from './components/FunctionalComponents/contact.jsx';
+import Signup from './components/FunctionalComponents/signup.jsx';
+import NavBar from './components/FunctionalComponents/navBar.jsx'; // Corrected path
+
+// Hooks Components
+import UseState from './components/FunctionalComponents/Hooks/useState.jsx';
+import UseEffect from './components/FunctionalComponents/Hooks/useEffect.jsx';
+import UseEffectApi from './components/FunctionalComponents/Hooks/useEffectApi.jsx';
+import UseReducer from './components/FunctionalComponents/Hooks/useReducer.jsx';
+import UseEffectApiImage from './components/FunctionalComponents/Hooks/useEffectApiimage.jsx';
+import UseRef from './components/FunctionalComponents/Hooks/useRef.jsx';
+import UseMemo from './components/FunctionalComponents/Hooks/useMemo.jsx';
+import UseCallBack from './components/FunctionalComponents/Hooks/useCallback.jsx';
+
+// Rendering the Application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Signup/>}></Route>
-      <Route path="/home" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/gallery" element={<Gallery/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-     
-      <Route path="/usestate" element={<UseState/>}></Route>
-      <Route path="/use-effect" element={<UseEffect/>}></Route>
-      <Route path="/use-effect-api" element={<UseEffectApi/>}></Route>
-      <Route path="/use-effect-Image" element={<UseEffectApiImage/>}></Route>
-      <Route path="/use-reducer" element={<UseReducer/>}></Route>
-      <Route path="/use-ref" element={<UseRef/>}></Route>
-      <Route path='/use-memo' element={<UseMemo/>}></Route>
-      <Route path='/use-callback' element={<UseCallBack/>}></Route>
-    </Routes>
-    
+      <NavBar /> {/* Include NavBar */}
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        
+        {/* Hooks Routes */}
+        <Route path="/usestate" element={<UseState />} />
+        <Route path="/use-effect" element={<UseEffect />} />
+        <Route path="/use-effect-api" element={<UseEffectApi />} />
+        <Route path="/use-effect-image" element={<UseEffectApiImage />} />
+        <Route path="/use-reducer" element={<UseReducer />} />
+        <Route path="/use-ref" element={<UseRef />} />
+        <Route path="/use-memo" element={<UseMemo />} />
+        <Route path="/use-callback" element={<UseCallBack />} />
+      </Routes>
     </BrowserRouter>
-    
-  </StrictMode>,
-)
+  </StrictMode>
+);
